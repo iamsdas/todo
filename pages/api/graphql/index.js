8eccb35867734ca474ad7e6af10955bc1609235e
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-06-08 15:52:26 +0530

    add some more resolvers

--- pages/api/graphql/index.js
+++ pages/api/graphql/index.js
@@ -17,6 +17,10 @@ const typeDefs = gql`
   type Mutation {
     addList(email: String!, title: String): List
     addItem(id: Int!, body: String): Item
+    updateList(id: Int!, title: String!): Int
+    updateItem(id: Int!, body: String!): Int
+    deleteList(id: Int!): Int
+    deleteItem(id: Int!): Int
   }
 `;
 
@@ -59,6 +63,56 @@ const resolvers = {
         },
       });
       return item;
+    },
+    updateItem: async (_, { id, body }, __, ___) => {
+      const item = await prisma.listItem.update({
+        where: {
+          id: id,
+        },
+        data: {
+          body: body,
+        },
+        select: {
+          id: true,
+        },
+      });
+      return item.id;
+    },
+    updateList: async (_, { id, title }, __, ___) => {
+      const list = await prisma.todoList.update({
+        where: {
+          id: id,
+        },
+        data: {
+          title: title,
+        },
+        select: {
+          id: true,
+        },
+      });
+      return list.id;
+    },
+    deleteItem: async (_, { id }, __, ___) => {
+      const item = await prisma.listItem.delete({
+        where: {
+          id: id,
+        },
+        select: {
+          id: true,
+        },
+      });
+      return item.id;
+    },
+    deleteList: async (_, { id }, __, ___) => {
+      const list = await prisma.todoList.delete({
+        where: {
+          id: id,
+        },
+        select: {
+          id: true,
+        },
+      });
+      return list.id;
     },
   },
 };
